6e977994402ac58b6c58d864a9fc33a4391d784a
Matheus Oliveira Mesquita <71468688+Harrys23@users.noreply.github.com>
2021-11-08 17:35:45 -0300

    Criação do componente Home

---
+++ components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex flex-wrap mt-20 gap-6 justify-center">
+		<div class="flex flex-col max-w-lg gap-6">
+			<CardPerfil />
+			<CardTrends />
+		</div>
+		<Feed />
+		<div class="flex flex-col gap-6">
+			<CardRecomend />
+			<About />
+		</div>
+	</div>
+</template>
+
+<style lang="scss"></style>
