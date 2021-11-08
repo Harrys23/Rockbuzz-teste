3dc63139b3d627a576f1b498bd424bba29d43922
Matheus Oliveira Mesquita <71468688+Harrys23@users.noreply.github.com>
2021-11-08 17:36:25 -0300

    Criação do componente About

---
+++ components/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="about rounded-2xl w-72 p-3 hidden xl:flex xl:flex-col">
+		© 2014 Twitter About HelpTerms Privacy Cookies Ads info Brand Blog Status
+		Apps Jobs Advertise Businesses Media Developers
+	</div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss">
+.about {
+	background-color: var(--xx-gray);
+	border: 1px solid var(--light-gray);
+}
+</style>
